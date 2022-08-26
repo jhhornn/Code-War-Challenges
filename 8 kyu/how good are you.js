4a6ab971fe7161917403a801b964d2d133ba3871
Awosise Oluwaseun <awosiseo@gmail.com>
2022-08-26 22:38:02 +0100

    how good are you

---
+++ 8 kyu/how good are you.js
@@ -0,0 +1,26 @@
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+// Solution
+
+function betterThanAverage(
+	classPoints,
+	yourPoints,
+) {
+	let average =
+		classPoints.reduce(
+			(prev, next) => prev + next,
+		) / classPoints.length;
+	if (yourPoints > average) {
+		return true;
+	} else {
+		return false;
+	}
+}
