9ce5387f7b82003524bb509f699e8a205d7fb032
Awosise Oluwaseun <awosiseo@gmail.com>
2022-09-02 22:42:12 +0100

    find the smallest integer in the array solution

--- 5 kyu/find the smallest integer in the array.js
+++ 5 kyu/find the smallest integer in the array.js
@@ -23,14 +23,20 @@ Pseudocode:
 // Solution
 class SmallestIntegerFinder {
 	findSmallestInt(args) {
-        
-	}
+        let min = args[0]
+        for (let i = 0; i < args.length; i++) {
+          if (args[i] < min) {
+            min = args[i]
+          }
+        }
+        return min
+    }
 }
 
 // Test Cases
-sif.findSmallestInt([78, 56, 232, 12, 8,])//"Should return the smallest int 8";
-sif.findSmallestInt([78, 56, 232, 12, 18,])//"Should return the smallest int 12";
-sif.findSmallestInt([78, 56, 232, 412, 228,])//Should return the smallest int 56";
-sif.findSmallestInt([78, 56, 232, 12, 0,])//"Should return the smallest int 0";
-sif.findSmallestInt([1, 56, 232, 12, 8,])//"Should return the smallest int 1";
+SmallestIntegerFinder.findSmallestInt([78, 56, 232, 12, 8,])//"Should return the smallest int 8";
+SmallestIntegerFinder.findSmallestInt([78, 56, 232, 12, 18,])//"Should return the smallest int 12";
+SmallestIntegerFinder.findSmallestInt([78, 56, 232, 412, 228,])//Should return the smallest int 56";
+SmallestIntegerFinder.findSmallestInt([78, 56, 232, 12, 0,])//"Should return the smallest int 0";
+SmallestIntegerFinder.findSmallestInt([1, 56, 232, 12, 8,])//"Should return the smallest int 1";
 
