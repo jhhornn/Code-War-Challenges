906bcf4e91943bb109c12fdfcc0fa17df5b70f2b
Awosise Oluwaseun <awosiseo@gmail.com>
2022-09-07 21:18:58 +0100

    replace with alphabet position solution

--- 6 kyu/replace with alphabet position.js
+++ 6 kyu/replace with alphabet position.js
@@ -11,4 +11,47 @@
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
 
 // Solution
+function alphabetPosition(text) {
+	let alphabet = [
+		"a",
+		"b",
+		"c",
+		"d",
+		"e",
+		"f",
+		"g",
+		"h",
+		"i",
+		"j",
+		"k",
+		"l",
+		"m",
+		"n",
+		"o",
+		"p",
+		"q",
+		"r",
+		"s",
+		"t",
+		"u",
+		"v",
+		"w",
+		"x",
+		"y",
+		"z",
+	];
+	let encoder = [];
+	let joinedStr = text.split().join("");
+	for (let c of joinedStr) {
+		c = c.toLowerCase();
+		if (alphabet.includes(c)) {
+			console.log(c);
+			encoder.push(
+				alphabet.indexOf(c) + 1,
+			);
+		}
+	}
+	return encoder.join(" ");
+}
+
 
