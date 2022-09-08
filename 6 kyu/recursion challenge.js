5711d262950108bbe6a0995d9be85d1099619680
Awosise Oluwaseun <awosiseo@gmail.com>
2022-09-09 00:02:46 +0100

    recursion challenge test cases

--- 6 kyu/recursion challenge.js
+++ 6 kyu/recursion challenge.js
@@ -1,10 +1,11 @@
 // Question
 
-// A function that calculates the factorial of a number and also 
+// A function that calculates the factorial of a number and also
 // read through a sring a return a string with replaced 3rd value.
-
 
 // Solution
 
-
-
+// Test Cases
+recursionChallenge(6); // returns 72XxfXxaXx2X;
+recursionChallenge(10); // returns 36X88X0xX1xX0xX8;
+recursionChallenge(20); // returns 24X29X20X81X66X00X0xX1xX0xX8;
