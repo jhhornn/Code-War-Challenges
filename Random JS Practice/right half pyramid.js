ef8944737ba0d0f182296ffd2faaafb5b8d9c5f8
Awosise Oluwaseun <awosiseo@gmail.com>
2023-01-19 16:38:08 +0100

    new: solution to the right angle pyramid problem

--- Random JS Practice/right half pyramid.js
+++ Random JS Practice/right half pyramid.js
@@ -2,3 +2,18 @@
 
 // A function that takes a number and returns a right half pyramid
 // with that number of rows
+
+// Solution
+
+function rightPyramid(num, symbol) {
+  let pyramid = ''
+  for (let i = 0; i <= num; i++) {
+    pyramid +=
+      '\n' +
+      ' '.repeat(num - i) +
+      symbol.toString().repeat(i)
+  }
+
+  return pyramid
+}
+
